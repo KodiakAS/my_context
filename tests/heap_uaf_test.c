116086d712b242e3610295f49fe532c9af38fe28
KodiakAS <38487204+KodiakAS@users.noreply.github.com>
2025-07-01 20:53:51 +0800

    Add ASan tests with yield scenarios

--- tests/heap_uaf_test.c
+++ tests/heap_uaf_test.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 
 static struct my_context ctx;
+static char *dangling;
 
 static void coroutine(void *arg) {
   (void)arg;
-  /* Intentional memory leak */
-  malloc(10);
+  char *p = malloc(8);
+  dangling = p;
+  free(p);
   my_context_yield(&ctx);
 }
 
 int main(void) {
   my_context_init(&ctx, 1 << 16);
   my_context_spawn(&ctx, coroutine, NULL);
+  /* Use after free */
+  dangling[0] = 'B';
   my_context_continue(&ctx);
   my_context_destroy(&ctx);
   return 0;
--- tests/stack_uaf_test.c
+++ tests/stack_uaf_test.c
@@ -8,15 +8,16 @@ static void coroutine(void *arg) {
   (void)arg;
   char local[8];
   dangling = local;
+  my_context_yield(&ctx);
 }
 
 int main(void) {
   my_context_init(&ctx, 1 << 16);
   my_context_spawn(&ctx, coroutine, NULL);
-  my_context_continue(&ctx);
-  my_context_destroy(&ctx);
+  my_context_continue(&ctx); /* finish coroutine */
   /* Stack use after return */
   dangling[0] = 'A';
   printf("%c\n", dangling[0]);
+  my_context_destroy(&ctx);
   return 0;
 }
